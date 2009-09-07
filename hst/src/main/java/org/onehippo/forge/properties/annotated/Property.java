6732579a72049cb1e177e31b1f90ba4345a61f14
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2009-09-07 14:44:44 +0000

    First commit of properties project

---
+++ hst/src/main/java/org/onehippo/forge/properties/annotated/Property.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2009 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.forge.properties.annotated;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+/**
+ * [properties:property] 
+ *   - properties:name (string)
+ *   - properties:value (string)
+ */
+@Node(jcrType = "properties:property")
+public class Property extends HippoDocument {
+
+    public String getName() {
+        return this.getProperty("properties:name");
+    }
+
+    public String getValue() {
+        return this.getProperty("properties:value");
+    }
+}
---
+++ hst/src/main/java/org/onehippo/forge/properties/bean/PropertiesBean.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2009 Hippo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onehippo.forge.properties.bean;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+import org.onehippo.forge.properties.annotated.Properties;
+import org.onehippo.forge.properties.annotated.Property;
+
+/**
+ * Bean representing a properties document that holds a map of name/value pairs.
+ * 
+ * In an HST Component, a bean like this can be created with a Properties object  
+ * as argument and subsequently set on the request for access in jsp.  
+ * 
+ * Example:
+ *    // get a document named "properties" at same level as current bean
+ *    HippoBean properties = this.getContentBean(request).getParentBean().getBean("properties"); 
+ *    
+ *    if (properties instanceof Properties) {
+ *        request.setAttribute("properties", new PropertiesBean(properties));
+ *    }   
+ *
+ * Because it is a map, access in jsp can be done using direct expression 
+ * language notation like "properties['label.header']"
+ */
+public class PropertiesBean implements Map {
+
+    private final Map<String,String> properties = new HashMap<String,String>();
+
+    public PropertiesBean(final Properties properties) {
+
+    	Iterator<Property> props = properties.getPropertyObjects().iterator();
+    	while (props.hasNext()) {
+    		Property prop = props.next();
+    		this.properties.put(prop.getName(), prop.getValue());
+    	}
+    }
+
+	public void clear() {
+		this.properties.clear();
+    }
+
+	public boolean containsKey(Object key) {
+	    return this.properties.containsKey(key);
+    }
+
+	public boolean containsValue(Object value) {
+	    return this.properties.containsValue(value);
+    }
+
+	public Set entrySet() {
+	    return this.properties.entrySet();
+    }
+
+	public Object get(Object key) {
+	    return this.properties.get(key);
+    }
+
+	public boolean isEmpty() {
+	    return this.properties.isEmpty();
+    }
+
+	public Set keySet() {
+	    return this.properties.keySet();
+    }
+
+	public Object put(Object arg0, Object arg1) {
+	    throw new UnsupportedOperationException("PropertiesBean is a readonly object");
+    }
+
+	public void putAll(Map arg0) {
+	    throw new UnsupportedOperationException("PropertiesBean is a readonly object");
+    }
+
+	public Object remove(Object arg0) {
+	    throw new UnsupportedOperationException("PropertiesBean is a readonly object");
+    }
+
+	public int size() {
+	    return this.properties.size();
+    }
+
+	public Collection values() {
+	    return this.properties.values();
+    }
+}
