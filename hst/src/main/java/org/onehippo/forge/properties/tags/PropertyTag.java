c4f777a2863932d20ac10a9f481adaf7779858d7
Lef Karamoulas <lef.karamoulas@bloomreach.com>
2026-07-17 15:38:52 +0300

    FORGE-595 : Update copyright, release notes, versions and minor cleanups

--- hst/src/main/java/org/onehippo/forge/properties/tags/PropertyTag.java
+++ hst/src/main/java/org/onehippo/forge/properties/tags/PropertyTag.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2024 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2026 Bloomreach Inc. (http://www.bloomreach.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-
 package org.onehippo.forge.properties.tags;
 
 import java.io.IOException;
